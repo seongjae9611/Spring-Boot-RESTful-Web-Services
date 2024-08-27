7cad51143d7b123c5e733e8be6bfe64b16160a59
나성재 <>
2024-08-28 03:54:31 +0900

    2강

---
+++ my-restful-service/my-restful-service/src/main/java/kr/co/seongjae/myrestfulservice/controller/UserController.java
@@ -0,0 +1,59 @@
+package kr.co.seongjae.myrestfulservice.controller;
+
+import kr.co.seongjae.myrestfulservice.bean.User;
+import kr.co.seongjae.myrestfulservice.dao.UserDaoService;
+import kr.co.seongjae.myrestfulservice.exception.UserNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserController {
+    private UserDaoService service;
+
+    public UserController(UserDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        User user = service.findOne(id);
+
+        if (user == null) {
+            throw new UserNotFoundException(String.format("id[%s] not found", id));
+        }
+
+        return user;
+    }
+
+    @PostMapping("/user")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity deleteUser(@PathVariable int id) {
+        User deleteUser = service.deleteById(id);
+
+        if (deleteUser == null) {
+            throw new UserNotFoundException(String.format("id[%s] not found", id));
+        }
+
+        return ResponseEntity.noContent().build();
+    }
+}
