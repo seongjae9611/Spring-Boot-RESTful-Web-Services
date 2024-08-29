60ab277bbda6cd00d2ef210a331022c56cfea857
나성재 <>
2024-08-30 08:32:03 +0900

    4강

--- my-restful-service/my-restful-service/src/main/java/kr/co/seongjae/myrestfulservice/controller/UserController.java
+++ my-restful-service/my-restful-service/src/main/java/kr/co/seongjae/myrestfulservice/controller/UserController.java
@@ -1,9 +1,17 @@
 package kr.co.seongjae.myrestfulservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+//import javax.validation.Valid;
 import kr.co.seongjae.myrestfulservice.bean.User;
 import kr.co.seongjae.myrestfulservice.dao.UserDaoService;
 import kr.co.seongjae.myrestfulservice.exception.UserNotFoundException;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -11,7 +19,11 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.List;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
+@Tag(name="user-controller", description = "일반 사용자 서비스를 위한 컨트롤러")
 public class UserController {
     private UserDaoService service;
 
@@ -24,15 +36,28 @@ public class UserController {
         return service.findAll();
     }
 
+    @Operation(summary = "사용자 정보 조회 API", description = "사용자 ID를 이용하여 사용자 상세 정보 조회를 합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = " BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "USER NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR"),
+    })
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id) {
+    public EntityModel<User> retrieveUser(
+            @Parameter(description = "사용자 ID", required = true, example = "1") @PathVariable int id) {
         User user = service.findOne(id);
 
         if (user == null) {
             throw new UserNotFoundException(String.format("id[%s] not found", id));
         }
 
-        return user;
+        EntityModel entityModel = EntityModel.of(user);
+
+        WebMvcLinkBuilder linTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(linTo.withRel("all-users")); // all=users -> http://localhost:8088/users
+
+        return entityModel;
     }
 
     @PostMapping("/user")
--- my-restful-service/my-restful-service/src/main/java/kr/co/seongjae/myrestfulservice/exception/CustomizedResponseEntityExceptionHandler.java
+++ my-restful-service/my-restful-service/src/main/java/kr/co/seongjae/myrestfulservice/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,8 +1,9 @@
 package kr.co.seongjae.myrestfulservice.exception;
 
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
+//import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
